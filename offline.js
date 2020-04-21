c7d1a73f34789c3d8486b8e12f97f787a8f40095
duoc.nguyen <duoc.nguyen@mtech.com.vn>
2020-04-21 18:40:03 +0700

    build html final

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1587469107,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/wheel-sheet0.png",
+		"images/pin-sheet0.png",
+		"images/spinbtn-sheet0.png",
+		"images/spinbtn-sheet1.png",
+		"images/bg1-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/replaybtn-sheet0.png",
+		"images/checkpoint-sheet0.png",
+		"images/particles.png",
+		"media/sound-correct.m4a",
+		"media/sound-correct.ogg",
+		"media/end_game.m4a",
+		"media/end_game.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
